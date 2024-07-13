a49ec259a6c2fa9d902879882d44804ee018c2d7
NishantSKumbhar <nishantsk210@gmail.com>
2024-07-14 00:17:06 +0530

    Created DbContext for the Project

---
+++ INDIA/Data/IndiaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using INDIA.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace INDIA.Data
+{
+    public class IndiaDbContext: DbContext
+    {
+        public IndiaDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<State> States { get; set; }
+    }
+}
