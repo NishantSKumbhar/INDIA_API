97a36479061a7171e255e2a79149d95c4f78d279
NishantSKumbhar <nishantsk210@gmail.com>
2024-07-13 22:56:46 +0530

    Created the Domain Models for the Project

---
+++ INDIA/Models/Domain/District.cs
@@ -0,0 +1,11 @@
+﻿namespace INDIA.Models.Domain
+{
+    public class District
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public double AreaInSqrKm { get; set; }
+        public string? DistrictImageUrl { get; set; }
+    }
+}
---
+++ INDIA/Models/Domain/State.cs
@@ -0,0 +1,19 @@
+﻿namespace INDIA.Models.Domain
+{
+    public class State
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double AreaInSqrKm { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string? StateImageUrl { get; set; }
+        public Guid DistrictId { get; set; }
+        public Guid LanguageId { get; set; }
+
+        // Navigation Properties
+        public District District { get; set; }
+        public Language Language { get; set; }
+
+    }
+}
